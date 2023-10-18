5f38ba72f7a40c4b884d9d00fda65591e349503a
Leeyah-123 <junaidaaliyah260@gmail.com>
2023-10-18 19:31:48 +0100

    chore: Metric Cards

--- src/components/simple/MetricCard.vue
+++ src/components/simple/MetricCard.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  metric: string
+  value: string
+}>()
+</script>
+
 <template>
-  wee
+  <div
+    class="flex justify-between bg-[linear-gradient(135deg,_#24303f_51%,_#aeb7c0_100%)] px-8 py-8 h-[150px] rounded-[20px] text-white">
+    <div>
+      <p className="text-xl md:text-lg lg:text-2xl mb-4">
+        {{ metric }}
+      </p>
+      <p>{{ value }}</p>
+    </div>
+    <div className="bg-secondary flex justify-center items-center p-2 w-[50px] h-[50px] rounded-[15px]">
+      <slot name="icon"></slot>
+    </div>
+  </div>
 </template>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,6 +1,60 @@
 <script setup lang="ts">
+import MetricCard from '@/components/simple/MetricCard.vue';
+import { useConsultantsStore } from '@/stores/consultants.store';
+import { useUsersStore } from '@/stores/users.store';
+import consultantRequests from '@/utils/apiRequests/consultant.requests';
+import userRequests from '@/utils/apiRequests/user.requests';
+import { Loading, Notify } from 'notiflix';
+import { computed, onMounted } from 'vue';
+
+const usersStore = useUsersStore()
+const users = computed(() => usersStore.users)
+const setUsers = usersStore.setUsers
+
+const consultantsStore = useConsultantsStore()
+const consultants = computed(() => consultantsStore.consultants)
+const setConsultants = consultantsStore.setConsultants
+
+onMounted(async () => {
+  if (!users.value) {
+    Loading.hourglass('Fetching users')
+    const response = await userRequests().getAllUsers()
+    if (response.success) {
+      setUsers(response.data)
+    } else Notify.failure(response.message)
+    Loading.remove()
+  }
+
+  if (!consultants.value) {
+    Loading.hourglass('Fetching consultants')
+    const response = await consultantRequests().getAllConsultants()
+    if (response.success) {
+      setConsultants(response.data)
+    } else Notify.failure(response.message)
+    Loading.remove()
+  }
+})
 </script>
 
 <template>
-  <h2>Home Page</h2>
+  <div class="grid sm:grid-cols-2 xl:grid-cols-3 gap-3">
+    <metric-card metric="Users" :value="users?.length.toString() || '0'">
+      <template #icon>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <title>account-multiple</title>
+          <path
+            d="M16 17V19H2V17S2 13 9 13 16 17 16 17M12.5 7.5A3.5 3.5 0 1 0 9 11A3.5 3.5 0 0 0 12.5 7.5M15.94 13A5.32 5.32 0 0 1 18 17V19H22V17S22 13.37 15.94 13M15 4A3.39 3.39 0 0 0 13.07 4.59A5 5 0 0 1 13.07 10.41A3.39 3.39 0 0 0 15 11A3.5 3.5 0 0 0 15 4Z" />
+        </svg>
+      </template>
+    </metric-card>
+    <metric-card metric="Consultants" :value="consultants?.length.toString() || '0'">
+      <template #icon>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <title>account-multiple</title>
+          <path
+            d="M16 17V19H2V17S2 13 9 13 16 17 16 17M12.5 7.5A3.5 3.5 0 1 0 9 11A3.5 3.5 0 0 0 12.5 7.5M15.94 13A5.32 5.32 0 0 1 18 17V19H22V17S22 13.37 15.94 13M15 4A3.39 3.39 0 0 0 13.07 4.59A5 5 0 0 1 13.07 10.41A3.39 3.39 0 0 0 15 11A3.5 3.5 0 0 0 15 4Z" />
+        </svg>
+      </template>
+    </metric-card>
+  </div>
 </template>
